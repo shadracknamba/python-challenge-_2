c4d65d1c2fc23913dc6571fac27f08b9411c1352
shadracknamba <120615872+shadracknamba@users.noreply.github.com>
2023-02-22 19:13:48 -0600

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,10 @@
+#importing required libraries 
+
+import pandas as pd
+import numpy as np
+import csv 
+from pathlib import Path # 
+
+budget_data = pd.read_csv('budget_data.csv')
+
+print('budget_data.csv')
